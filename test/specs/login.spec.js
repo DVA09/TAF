878ee0d6da5f62d08a7a04111cf59c86be4947bc
Vladimir Deushev <vladimir.deushev@innowise.com>
2024-02-06 12:47:33 +0300

    POM delete report folder and comment

--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -8,15 +8,5 @@ describe("Sauce Tests", () => {
     await LoginPage.login("standard_user", "secret_sauce");
 
     await LoginPage.checkTitle('Swag Labs');
-
-    // await $("#user-name").setValue("standard_user");
-
-    // await $("#password").setValue("secret_sauce");
-
-    // await $("#login-button").click();
-
-    // await $('.app_logo').getText();
-
-    // await expect($('.app_logo')).toHaveTextContaining('Swag Labs');
   });
 });
