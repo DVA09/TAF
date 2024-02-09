c94444c5a67554d8fcd6cb342058c9a060a2d85a
Vladimir Deushev <vladimir.deushev@innowise.com>
2024-02-09 10:19:41 +0300

    POM add inventory card and footer tests for the Product page

--- test/pages/ProductPage.js
+++ test/pages/ProductPage.js
@@ -5,17 +5,25 @@ class ProductPage {
 
         return $("//*[@id='react-burger-menu-btn']");
     }
+
     get iconBurgerMenuItem1() {
         return $("//*[@id='inventory_sidebar_link']");
     }
+
     get iconBurgerMenuItem2() {
         return $("//*[@id='about_sidebar_link']");
     }
+
     get iconBurgerMenuItem3() {
         return $("//*[@id='logout_sidebar_link']");
     }
+
     get iconBurgerMenuItem4() {
         return $("//*[@id='reset_sidebar_link']");
+    }
+    
+    get iconBurgerMenuCross() {
+        return $("//*[@id='react-burger-cross-btn']");
     }
     
     get homeTitle() {
@@ -83,7 +91,7 @@ class ProductPage {
     }
 
     //Footer locators
-    get socialTwitter() {
+    get social() {
         return $$(".social");
     }
 
