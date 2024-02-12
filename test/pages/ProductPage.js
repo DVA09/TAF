64214c71561b8dacc5aa7f72d3e1d795071810f8
Vladimir Deushev <vladimir.deushev@innowise.com>
2024-02-12 11:58:30 +0300

    POM add tests for footer of Home page

--- test/pages/ProductPage.js
+++ test/pages/ProductPage.js
@@ -1,104 +1,111 @@
 class ProductPage {
+  //Header locators
+  get iconBurgerMenu() {
+    return $("//*[@id='react-burger-menu-btn']");
+  }
 
-    //Header locators
-    get iconBurgerMenu() {
+  get iconBurgerMenuItem1() {
+    return $("//*[@id='inventory_sidebar_link']");
+  }
 
-        return $("//*[@id='react-burger-menu-btn']");
-    }
+  get iconBurgerMenuItem2() {
+    return $("//*[@id='about_sidebar_link']");
+  }
 
-    get iconBurgerMenuItem1() {
-        return $("//*[@id='inventory_sidebar_link']");
-    }
+  get iconBurgerMenuItem3() {
+    return $("//*[@id='logout_sidebar_link']");
+  }
 
-    get iconBurgerMenuItem2() {
-        return $("//*[@id='about_sidebar_link']");
-    }
+  get iconBurgerMenuItem4() {
+    return $("//*[@id='reset_sidebar_link']");
+  }
 
-    get iconBurgerMenuItem3() {
-        return $("//*[@id='logout_sidebar_link']");
-    }
+  get iconBurgerMenuCross() {
+    return $("//*[@id='react-burger-cross-btn']");
+  }
 
-    get iconBurgerMenuItem4() {
-        return $("//*[@id='reset_sidebar_link']");
-    }
-    
-    get iconBurgerMenuCross() {
-        return $("//*[@id='react-burger-cross-btn']");
-    }
-    
-    get homeTitle() {
-        return $(".app_logo");
-    }
+  get homeTitle() {
+    return $(".app_logo");
+  }
 
-    get iconShoppingBasket() {
-        return $("//*[@id='shopping_cart_container']/a");
-    }
+  get iconShoppingBasket() {
+    return $("//*[@id='shopping_cart_container']/a");
+  }
 
-    //Header secondary locators
-    get headerTitle() {
-        return $("//*[@id='header_container']/div[2]/span");
-    }
+  //Header secondary locators
+  get headerTitle() {
+    return $("//*[@id='header_container']/div[2]/span");
+  }
 
-    get activeOption() {
-        return $("//*[@id='header_container']/div[2]/div/span/span");
-    }
+  get activeOption() {
+    return $("//*[@id='header_container']/div[2]/div/span/span");
+  }
 
-    get productSort() {
-        return $("//*[@id='header_container']/div[2]/div/span/select");
-    }
-    
-    get activeOption() {
-        return $("//*[@id='header_container']/div[2]/div/span/span");
-    }
+  get productSort() {
+    return $("//*[@id='header_container']/div[2]/div/span/select");
+  }
 
-    get sortOption1() {
-        return $("//*[@id='header_container']/div[2]/div/span/select/option[1]");
-    }
+  get activeOption() {
+    return $("//*[@id='header_container']/div[2]/div/span/span");
+  }
 
-    get sortOption2() {
-        return $("//*[@id='header_container']/div[2]/div/span/select/option[2]");
-    }
+  get sortOption1() {
+    return $("//*[@id='header_container']/div[2]/div/span/select/option[1]");
+  }
 
-    get sortOption3() {
-        return $("//*[@id='header_container']/div[2]/div/span/select/option[3]");
-    }
+  get sortOption2() {
+    return $("//*[@id='header_container']/div[2]/div/span/select/option[2]");
+  }
 
-    get sortOption4() {
-        return $("//*[@id='header_container']/div[2]/div/span/select/option[4]");
-    }
+  get sortOption3() {
+    return $("//*[@id='header_container']/div[2]/div/span/select/option[3]");
+  }
 
+  get sortOption4() {
+    return $("//*[@id='header_container']/div[2]/div/span/select/option[4]");
+  }
 
+  //Product card locators
+  get productList() {
+    return $("//*[@id='inventory_container']/div");
+  }
 
-    //Product card locators
-    get productList() {
-        return $("//*[@id='inventory_container']/div");
-    }
+  get productName() {
+    return $$(".inventory_item_name");
+  }
 
-    get productName() {
-        return $$(".inventory_item_name");
-    }
+  get productDescription() {
+    return $$(".inventory_item_description");
+  }
 
-    get productDescription() {
-        return $$(".inventory_item_description");
-    }
+  get productImage() {
+    return $$(".inventory_item_img");
+  }
 
-    get productImage() {
-        return $$(".inventory_item_img");
-    }
+  get productBtn() {
+    return $$(".btn_inventory");
+  }
 
-    get productBtn() {
-        return $$(".btn_inventory");
-    }
+  //Footer locators
+  get social() {
+    return $$(".social");
+  }
 
-    //Footer locators
-    get social() {
-        return $$(".social");
-    }
+  get socialTwitter() {
+    return $("//*[@id='page_wrapper']/footer/ul/li[1]/a");
+  }
 
-    get copyText() {
-        return $$("//*[@id='page_wrapper']/footer/div");
-    }
-    
+  get socialFacebook() {
+    return $("//*[@id='page_wrapper']/footer/ul/li[2]/a");
+  }
+
+  get socialLinkedin() {
+    return $("//*[@id='page_wrapper']/footer/ul/li[3]/a");
+  }
+
+  get copyText() {
+    return $$("//*[@id='page_wrapper']/footer/div");
+  }
 }
 
 module.exports = new ProductPage();
--- test/specs/product_page.spec.js
+++ test/specs/product_page.spec.js
@@ -4,155 +4,148 @@ const login = require("../utils/login.json");
 const product = require("../utils/product.json");
 
 describe("Product Page", () => {
-    //Header tests
-    it("Burger Menu test", async () => {
+  //Header tests
+  it("Burger Menu test", async () => {
+    browser.url(product.url);
 
-        browser.url(product.url);
+    await LoginPage.loginCSS(login.username[0], login.password);
 
-        await LoginPage.loginCSS(login.username[0], login.password);
+    await expect(ProductPage.iconBurgerMenu).toBeClickable();
+  });
 
-        await expect(ProductPage.iconBurgerMenu).toBeClickable();
+  it("Burger Menu item1 test", async () => {
+    await ProductPage.iconBurgerMenu.click();
 
-    });
+    await expect(ProductPage.iconBurgerMenuItem1).toHaveText("All Items");
 
-    it("Burger Menu item1 test", async () => {
+    await expect(ProductPage.iconBurgerMenuItem1).toBeClickable();
+  });
 
-        await ProductPage.iconBurgerMenu.click();
+  it("Burger Menu item2 test", async () => {
+    await expect(ProductPage.iconBurgerMenuItem2).toHaveText("About");
 
-        await expect(ProductPage.iconBurgerMenuItem1).toHaveText('All Items');
+    await expect(ProductPage.iconBurgerMenuItem2).toBeClickable();
+  });
 
-        await expect(ProductPage.iconBurgerMenuItem1).toBeClickable();
+  it("Burger Menu item3 test", async () => {
+    await expect(ProductPage.iconBurgerMenuItem3).toHaveText("Logout");
 
-    });
+    await expect(ProductPage.iconBurgerMenuItem3).toBeClickable();
+  });
 
-    it("Burger Menu item2 test", async () => {
+  it("Burger Menu item4 test", async () => {
+    await expect(ProductPage.iconBurgerMenuItem4).toHaveText("Reset App State");
 
-        await expect(ProductPage.iconBurgerMenuItem2).toHaveText('About');
+    await expect(ProductPage.iconBurgerMenuItem4).toBeClickable();
 
-        await expect(ProductPage.iconBurgerMenuItem2).toBeClickable();
+    await ProductPage.iconBurgerMenuCross.click();
+  });
 
-    });
+  it("Product page title test", async () => {
+    await expect(ProductPage.homeTitle).toHaveText("Swag Labs");
+  });
 
-    it("Burger Menu item3 test", async () => {
+  it("Product basket test", async () => {
+    await expect(ProductPage.iconShoppingBasket).toBeDisplayed();
 
-        await expect(ProductPage.iconBurgerMenuItem3).toHaveText('Logout');
+    await expect(ProductPage.iconShoppingBasket).toBeClickable();
+  });
 
-        await expect(ProductPage.iconBurgerMenuItem3).toBeClickable();
+  //Header secondary tests
+  it("Header title test", async () => {
+    await expect(ProductPage.headerTitle).toHaveText("Products");
+  });
 
-    });
+  it("Filter test", async () => {
+    await expect(ProductPage.productSort).toBeDisplayed();
 
-    it("Burger Menu item4 test", async () => {
+    await expect(ProductPage.productSort).toBeClickable();
 
-        await expect(ProductPage.iconBurgerMenuItem4).toHaveText('Reset App State');
+    await expect(ProductPage.productSort).toHaveChildren(4);
 
-        await expect(ProductPage.iconBurgerMenuItem4).toBeClickable();
+    await ProductPage.productSort.click();
+  });
 
-        await ProductPage.iconBurgerMenuCross.click();
+  it("Filter Option test 1", async () => {
+    await expect(ProductPage.sortOption1).toBeDisplayed();
 
-    });
+    await expect(ProductPage.activeOption).toHaveText("Name (A to Z)");
+  });
 
-    it("Product page title test", async () => {
+  it("Filter Option test 2", async () => {
+    await ProductPage.sortOption2.click();
 
-        await expect(ProductPage.homeTitle).toHaveText('Swag Labs');
+    await expect(ProductPage.activeOption).toHaveText("Name (Z to A)");
 
-    });
+    await expect(ProductPage.sortOption2).toBeDisplayed();
+  });
 
-    it("Product basket test", async () => {
+  it("Filter Option test 3", async () => {
+    await ProductPage.productSort.click();
 
-        await expect(ProductPage.iconShoppingBasket).toBeDisplayed();
+    await ProductPage.sortOption3.click();
 
-        await expect(ProductPage.iconShoppingBasket).toBeClickable();
+    await expect(ProductPage.sortOption3).toBeDisplayed();
 
-    });
+    await expect(ProductPage.activeOption).toHaveText("Price (low to high)");
+  });
 
-    //Header secondary tests
-    it("Header title test", async () => {
+  it("Filter Option test 4", async () => {
+    await ProductPage.productSort.click();
 
-        await expect(ProductPage.headerTitle).toHaveText('Products');
+    await ProductPage.sortOption4.click();
 
-    });
-    
-    it("Filter test", async () => {
+    await expect(ProductPage.sortOption4).toBeDisplayed();
 
-        await expect(ProductPage.productSort).toBeDisplayed();        
-        
-        await expect(ProductPage.productSort).toBeClickable();
-        
-        await expect(ProductPage.productSort).toHaveChildren(4);
+    await expect(ProductPage.activeOption).toHaveText("Price (high to low)");
+  });
 
-        await ProductPage.productSort.click();
+  it("Inventory card", async () => {
+    await expect(ProductPage.productImage).toBeClickable();
 
-    });
+    await expect(ProductPage.productImage).toBeClickable();
 
-    it("Filter Option test 1", async () => {
+    await expect(ProductPage.productImage).toBeClickable();
+  });
 
-        await expect(ProductPage.sortOption1).toBeDisplayed();
-        
-        await expect(ProductPage.activeOption).toHaveText('Name (A to Z)');
+  //Footer
+  it("Footer social", async () => {
+    await expect(ProductPage.social).toBeClickable();
+  });
 
-    });
+  t("Footer social Twitter", async () => {
+    await ProductPage.socialTwitter.click();
 
-    it("Filter Option test 2", async () => {        
-        
-        await ProductPage.sortOption2.click();        
+    await expect(browser).toHaveUrl("https://twitter.com/saucelabs");
 
-        await expect(ProductPage.activeOption).toHaveText('Name (Z to A)');
+    await browser.switchWindow("https://www.saucedemo.com/inventory.html");
+  });
 
-        await expect(ProductPage.sortOption2).toBeDisplayed();
+  it("Footer social Facebook", async () => {
+    await ProductPage.socialFacebook.click();
 
-    });
+    await expect(browser).toHaveUrl("https://www.facebook.com/saucelabs");
 
-    it("Filter Option test 3", async () => {
+    await browser.switchWindow("https://www.saucedemo.com/inventory.html");
+  });
 
-        await ProductPage.productSort.click();
+  it("Footer social Linkedin", async () => {
+    await ProductPage.socialLinkedin.click();
 
-        await ProductPage.sortOption3.click();
+    await expect(browser).toHaveUrlContaining("https://www.linkedin.com/");
 
-        await expect(ProductPage.sortOption3).toBeDisplayed();        
+    await browser.switchWindow("https://www.saucedemo.com/inventory.html");
+  });
 
-        await expect(ProductPage.activeOption).toHaveText('Price (low to high)');
+  it("Footer copy", async () => {
+    await expect(ProductPage.copyText).toHaveText(
+      "© 2024 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy"
+    );
+  });
 
-    });
+  it("Log out", async () => {
+    await ProductPage.iconBurgerMenu.click();
 
-    it("Filter Option test 4", async () => {
-
-        await ProductPage.productSort.click();
-
-        await ProductPage.sortOption4.click();
-
-        await expect(ProductPage.sortOption4).toBeDisplayed();        
-
-        await expect(ProductPage.activeOption).toHaveText('Price (high to low)');
-
-    });
-    
-    it("Inventory card", async () => {
-
-        await expect(ProductPage.productImage).toBeClickable();
-
-        await expect(ProductPage.productImage).toBeClickable();
-
-        await expect(ProductPage.productImage).toBeClickable();
-
-    });
-    
-    it("Footer social", async () => {
-
-        await expect(ProductPage.social).toBeClickable();        
-
-    });
-    
-    it("Footer copy", async () => {
-
-        await expect(ProductPage.copyText).toHaveText("© 2024 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy");        
-
-    });
-
-    it("Log out", async () => {
-
-        await ProductPage.iconBurgerMenu.click();
-        
-        await ProductPage.iconBurgerMenuItem3.click();
-
-    });
-})
+    await ProductPage.iconBurgerMenuItem3.click();
+  });
+});
