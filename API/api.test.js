d019d5eef7cfe71212a0a7d6253db725adefdba3
Vladimir Deushev <vladimir.deushev@innowise.com>
2024-02-26 15:43:19 +0300

    feat: add create a new user test

--- API/api.test.js
+++ API/api.test.js
@@ -1,1 +1,17 @@
 const axios = require('axios');
+
+test('Create a new user', async () => {
+    const user = {
+        "id": 0,
+        "username": "Test UserLastName",
+        "firstName": "Test",
+        "lastName": "UserLastName",
+        "email": "testuser@example.com",
+        "password": "test12356_)",
+        "phone": "+375297891234",
+        "userStatus": 0
+    };
+    const response = await axios.post('https://petstore.swagger.io/v2/user', user);
+    expect(response.status).toBe(200);
+    console.log(response);
+});
