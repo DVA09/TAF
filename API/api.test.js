ef5ba0f90aab871a138637753f860aa61fbc192d
Vladimir Deushev <vladimir.deushev@innowise.com>
2024-02-27 12:09:40 +0300

    add tests for body properties

--- API/api.test.js
+++ API/api.test.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 test('Create a new user', async () => {
     const user = {
-        "id": 0,
+        "id": 22,
         "username": "Test UserLastName",
         "firstName": "Test",
         "lastName": "UserLastName",
@@ -13,11 +13,15 @@ test('Create a new user', async () => {
     };
     const response = await axios.post('https://petstore.swagger.io/v2/user', user);
     expect(response.status).toBe(200);
+    expect(response.data.type).toBe("unknown");
+    expect(response.data.message).toBe("22");    
 });
 
 test('User with ID 0 exists', async () => {
     const response = await axios.get('https://petstore.swagger.io/v2/user/login?username=Test%20UserLastName&password=test12356_)');
     expect(response.status).toBe(200);
+    expect(response.data.type).toBe("unknown");
+    expect(response.data.message).toBe("logged in user session:1709024419211");        
 });
 
 test('Updated user', async () => {
@@ -25,11 +29,15 @@ test('Updated user', async () => {
         username: "New User"
     });
     expect(response.status).toBe(200);
+    expect(response.data.type).toBe("unknown");
+    expect(response.data.message).toBe("9223372036854775807");    
 });
 
-test('Updated user', async () => {
+test('Delete user', async () => {
     const response = await axios.delete('https://petstore.swagger.io/v2/user/New%20User', {
         username: "New User"
     });
     expect(response.status).toBe(200);
+    expect(response.data.type).toBe("unknown");
+    expect(response.data.message).toBe("New User");
 });
